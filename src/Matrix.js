fe91b877020926a740340e8fcf649ce88ce9292d
Edward Sparks <edd_sparks@hotmail.com>
2018-10-29 17:10:54 +0000

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Matrix extends Component {
-  
+
+  static defaultProps = {
+    values: [
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO'],
+    ['#FOO', '#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO','#FOO']
+    ]
+  }
+
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
+    vals.map(val => <Cell value={val} />)
+    )
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
-  
+    )
+
   render() {
     return (
       <div id="matrix">
-        {this.genMatrix()}
+      {this.genMatrix()}
       </div>
-    )
+      )
   }
-  
+
 }
+
